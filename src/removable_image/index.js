4626feb0f602afc12135178974a3c6b6f50f499c
UladKasach <nappa300@gmail.com>
2018-05-28 16:52:19 -0400

    begin loading of removable_image view asap to reduce lag

--- src/removable_image/index.js
+++ src/removable_image/index.js
@@ -1,6 +1,7 @@
 var view_loader = require("clientside-view-loader");
+var this_dir = window.script_location.origin + window.script_location.pathdir;
+view_loader.load(this_dir) // begin async loading asap
 module.exports = async function(build_options){
-    var this_dir = window.script_location.origin + window.script_location.pathdir;
     var dom = await view_loader.load(this_dir).build(build_options);
     return dom;
 }
